84b17e486e23d2d2e7c6fe2d64da92262c165f67
jesus <jmlopezfabian@gmail.com>
2025-06-16 20:08:16 -0600

    Añadiendo botón para subir imagen

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import cv2
+import numpy as np
 from pyspark.sql import SparkSession
 
-st.write("BigData")
+
+st.title("Procesamiento Paralelo para la Detección de Bordes en Imágenes Binarias usando PySpark, OpenCV y Shiny")
+
+
+st.title("Sube una imagen")
+uploaded_file = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+    st.image(img, caption="Imagen cargada", use_column_width=True)
